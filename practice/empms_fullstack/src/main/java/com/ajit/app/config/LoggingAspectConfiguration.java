6d4c351f1852a1d55a9484f2504d6b829ad79562
x088082 <Ajit.Giri@klm.com>
2022-02-11 17:00:54 +0100

    Initial version of empms_fullstack generated by generator-jhipster@7.6.0

---
+++ practice/empms_fullstack/src/main/java/com/ajit/app/config/LoggingAspectConfiguration.java
@@ -0,0 +1,17 @@
+package com.ajit.app.config;
+
+import com.ajit.app.aop.logging.LoggingAspect;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import tech.jhipster.config.JHipsterConstants;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    @Profile(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
+    public LoggingAspect loggingAspect(Environment env) {
+        return new LoggingAspect(env);
+    }
+}
---
+++ practice/empms_fullstack/src/main/java/com/ajit/app/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.ajit.app.repository;
+
+import com.ajit.app.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
