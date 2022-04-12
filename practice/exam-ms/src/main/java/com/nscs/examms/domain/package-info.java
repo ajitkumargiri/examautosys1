b83e23056cdf1bda8197d323f11057f5547d04ef
x088082 <Ajit.Giri@klm.com>
2022-04-12 15:18:00 +0200

    Initial version of jhipster generated by generator-jhipster@7.8.1

---
+++ practice/exam-ms/src/main/java/com/nscs/examms/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JPA domain objects.
+ */
+package com.nscs.examms.domain;
---
+++ practice/exam-ms/src/main/java/com/nscs/examms/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.nscs.examms.web.rest.vm;
